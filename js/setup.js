ec95332b7b26dcc484f4cd2c1cae5e6aace3b4c4
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-02-29 12:29:30 +0300

    Удаление лишнего.

--- js/setup.js
+++ js/setup.js
@@ -2,10 +2,6 @@
 (function () {
   var wizardsParameters = window.wizardsParameters;
   var similarWizardsCount = wizardsParameters.count;
-  /* var wizardFirstName = wizardsParameters.names.FIRST;
-  var wizardSecondName = wizardsParameters.names.SECOND;
-  var wizardCoatColors = wizardsParameters.elementColors.COAT;
-  var wizardEyesColors = wizardsParameters.elementColors.EYES; */
 
   var userDialog = document.querySelector('.setup'); // Окно настройки персонажа
   var similarListElement = userDialog.querySelector('.setup-similar-list'); // Список похожих персонажей
@@ -15,18 +11,6 @@
       .content
       .querySelector('.setup-similar-item');
 
-  //  Функция создания массива волшебников
-  /* var wizardsCreate = function (wizardsCount) {
-    var wizardsArray = [];
-    for (var i = 0; i < wizardsCount; i++) {
-      wizardsArray.push({
-        name: window.random.arrayElement(wizardFirstName) + ' ' + window.random.arrayElement(wizardSecondName),
-        coatColor: window.random.arrayElement(wizardCoatColors),
-        eyesColor: window.random.arrayElement(wizardEyesColors)
-      });
-    }
-    return wizardsArray;
-  }; */
 
   //  Функция отрисовки волшебника
   var renderWizard = function (wizard) {
@@ -45,9 +29,6 @@
     }
     return fragment;
   };
-
-  /* var wizards = wizardsCreate(similarWizardsCount);
-  similarListElement.appendChild(renderWizards(wizards)); */
 
   var onBackendLoad = function (wizards) {
     similarListElement.appendChild(renderWizards(wizards));
